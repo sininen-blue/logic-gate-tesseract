6b5f51630c73cfe66133564f855dc4b7cd8bdb92
sininen-blue <108949710+sininen-blue@users.noreply.github.com>
2024-10-06 19:54:01 +0800

    cleanup

--- simulation/data_manager.gd
+++ simulation/data_manager.gd
@@ -1,6 +1,6 @@
 extends Node
 
-## TODO NOTE: this is a debug default value, remove after
+## TODO NOTE: this is a debug default value
 var current_stage : DirAccess = DirAccess.open("res://levels/basic_concepts")
 var current_level : JSON
 var is_custom : bool = false
