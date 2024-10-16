20ee212d8c169fcafdbde1381ef9e5bd45873d47
sininen-blue <108949710+sininen-blue@users.noreply.github.com>
2024-10-16 19:05:18 +0800

    an attempt that works kinda

--- ocr/ocr.py
+++ ocr/ocr.py
@@ -12,28 +12,43 @@ from PIL import Image
 # read each cell
 # return a json with a ["001", "011", ...] output
 
+
 custom_config = r'--psm 10 --oem 3'
-
 image_path = "test.png"
-img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-
-height, width = img.shape
-cell_height, cell_width = height // 9, width // 4
-
-table = []
-
-for i in range(9):  # row
-    row = []
-    for j in range(4):  # colum
-        cell = img[i * cell_height:(i + 1) * cell_height,
-                   j * cell_width:(j + 1) * cell_width]
-
-        # cv2.imwrite(f"cell_{i}_{j}.png", cell)
-
-        text = pytesseract.image_to_string(cell, config=custom_config)
-        row.append(text)
-    table.append(row)
 
 
-for row in table:
-    print(row)
+def main():
+    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+    roi = cv2.selectROI("Select Region", img,
+                        showCrosshair=True, fromCenter=False)
+    x, y, w, h = roi
+    cropped_img = img[int(y):int(y + h), int(x):int(x + w)]
+
+    cv2.imshow("Cropped Image", cropped_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    height, width = cropped_img.shape
+    cell_height, cell_width = height // 9, width // 4
+
+    table = []
+
+    for i in range(9):  # row
+        row = []
+        for j in range(4):  # colum
+            cell = img[i * cell_height:(i + 1) * cell_height,
+                       j * cell_width:(j + 1) * cell_width]
+
+            # cv2.imwrite(f"cell_{i}_{j}.png", cell)
+
+            text = pytesseract.image_to_string(cell, config=custom_config)
+            row.append(text)
+        table.append(row)
+
+    for row in table:
+        print(row)
+
+
+if __name__ == "__main__":
+    main()
