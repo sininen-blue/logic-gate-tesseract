364933770a2b6eb5c3013967d7c9b28c7426a2a2
sininen-blue <108949710+sininen-blue@users.noreply.github.com>
2024-10-25 14:34:29 +0800

    made custom levels file generate in user://

--- ui/title_page.gd
+++ ui/title_page.gd
@@ -5,7 +5,15 @@ extends Control
 @onready var settings_button: Button = $SettingsButton
 
 func _ready() -> void:
+	generate_files()
 	play_button.pressed.connect(_on_button_pressed)
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://ui/stage_select.tscn")
+
+
+func generate_files() -> void:
+	## TODO: this should be where i turn all the res:// files into user:// files
+	var exists: bool = DirAccess.dir_exists_absolute("user://custom_levels/")
+	if exists == false:
+		DirAccess.make_dir_absolute("user://custom_levels/")
