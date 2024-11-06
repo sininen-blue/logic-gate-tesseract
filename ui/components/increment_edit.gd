7a79575fb6b93c82d9158c90f5afeae7ba824544
sininen-blue <108949710+sininen-blue@users.noreply.github.com>
2024-11-06 14:04:07 +0800

    now properly has int count value

--- ui/components/increment_edit.gd
+++ ui/components/increment_edit.gd
@@ -1,7 +1,18 @@
 extends LineEdit
 class_name IncrementEdit
 
-@export var count : int = 0
+signal count_changed(new_count: int)
+
+@export var count : int = 0:
+	get:
+		return count
+	set(new_value):
+		count = new_value
+		self.text = str(count)
+		count_changed.emit(count)
+
+func _ready() -> void:
+	self.text = str(count)
 
 func _on_text_changed(new_text: String) -> void:
 	if new_text.is_empty():
@@ -18,7 +29,7 @@ func _on_increment_button_pressed() -> void:
 
 
 func _on_decrement_button_pressed() -> void:
-	count -= 0
+	count -= 1
 
 
 func error() -> void:
