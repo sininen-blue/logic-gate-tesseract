4e809af73ddb3c88063aa38cd78c77111cd691c9
sininen-blue <108949710+sininen-blue@users.noreply.github.com>
2024-11-08 21:24:25 +0800

    stage select revamped

--- ui/stage_select/stage_panel.gd
+++ ui/stage_select/stage_panel.gd
@@ -1,37 +1,29 @@
 extends Panel
 
-signal pressed()
+const LEVEL_SCENE: String = "uid://yrih2e5sant0"
+const CUSTOM_LEVEL_SCENE: String = "uid://bwlvlfljyekts"
 
-@export var title: String
+@export var level_dict: Dictionary
 @export var completed_levels: int
-@export var max_level: int
-
-const level_scene: String = "uid://yrih2e5sant0"
-var custom_level_scene: String = "uid://bwlvlfljyekts"
+@export var total_levels: int
 
 var dir : DirAccess
 
-
-func _on_gui_input(event: InputEvent) -> void:
-	if event.is_action_pressed("left_click"):
-		pressed.emit()
-
-func _on_mouse_entered() -> void:
-	self.mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
-
-func _on_mouse_exited() -> void:
-	self.mouse_default_cursor_shape = Control.CURSOR_ARROW
+@onready var title_label: Label = $TitleLabel
+@onready var description_label: Label = $ScrollContainer/DescriptionLabel
+@onready var progress_bar: ProgressBar = $ProgressBar
 
 
 func _ready() -> void:
-	$StageLabel.text = title
-	$ProgressLabel.text = str(completed_levels, "/", max_level)
-	$PlayButton.pressed.connect(play_level)
+	title_label.text = level_dict["title"]
+	description_label.text = level_dict["description"]
+	progress_bar.value = completed_levels
+	progress_bar.max_value = total_levels
 
 
-func play_level() -> void:
+func _on_play_button_pressed() -> void:
 	DataManager.current_stage = dir
-	if title == "Custom Levels":
-		get_tree().change_scene_to_file(custom_level_scene)
+	if level_dict["title"] == "Custom Levels":
+		get_tree().change_scene_to_file(CUSTOM_LEVEL_SCENE)
 	else:
-		get_tree().change_scene_to_file(level_scene)
+		get_tree().change_scene_to_file(LEVEL_SCENE)
--- ui/stage_select/stage_select.gd
+++ ui/stage_select/stage_select.gd
@@ -1,12 +1,10 @@
 extends Control
 
+const STAGE_PANEL: PackedScene = preload("uid://cj2adegb284b8")
+
 var stages_directory : DirAccess = DirAccess.open("user://levels/")
 
-@onready var stage_panel: PackedScene = preload("uid://cj2adegb284b8")
-
-@onready var stages_container: VBoxContainer = $Main/Stages
-@onready var title_label: Label = $Main/InfoPanel/TitleLabel
-@onready var description_label: Label = $Main/InfoPanel/DescriptionLabel
+@onready var stages_container: HBoxContainer = %Main
 
 
 func _on_back_button_pressed() -> void:
@@ -16,7 +14,6 @@ func _on_back_button_pressed() -> void:
 func _ready() -> void:
 	# ensure custom levels is at the top
 	make_panel("custom_levels")
-	
 	for stage in stages_directory.get_directories():
 		if stage == "custom_levels":
 			continue
@@ -25,15 +22,15 @@ func _ready() -> void:
 
 func make_panel(stage : String) -> void:
 	var stage_file : FileAccess = FileAccess.open("user://"+stage+".json", FileAccess.READ)
-	var content : Dictionary = JSON.parse_string(stage_file.get_as_text())
 	
-	var new_stage_panel : Panel = stage_panel.instantiate()
-	new_stage_panel.title = content["title"]
+	var new_stage_panel : Panel = STAGE_PANEL.instantiate()
 	
-	## TODO: player local save
+	new_stage_panel.level_dict = JSON.parse_string(stage_file.get_as_text())
+	
 	var stage_dir: DirAccess = DirAccess.open("user://levels/"+stage)
 	var stage_levels: Array = stage_dir.get_files()
-	new_stage_panel.max_level = len(stage_levels)
+	
+	new_stage_panel.total_levels = len(stage_levels)
 	
 	var count: int = 0
 	for level: String in stage_levels:
@@ -45,9 +42,3 @@ func make_panel(stage : String) -> void:
 	
 	new_stage_panel.dir = DirAccess.open("user://levels/"+stage)
 	stages_container.add_child(new_stage_panel)
-	new_stage_panel.pressed.connect(show_info.bind(content))
-
-
-func show_info(content : Dictionary) -> void:
-	title_label.text = content["title"]
-	description_label.text = content["description"]
