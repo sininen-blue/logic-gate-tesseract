c03f3f284678896471047057b8ed08ccd10ab437
sininen-blue <108949710+sininen-blue@users.noreply.github.com>
2024-11-23 15:57:06 +0800

    truth table row modified to have checks

--- simulation/side_truth_table.gd
+++ simulation/side_truth_table.gd
@@ -1,13 +1,53 @@
 extends Panel
 
+const TRUTH_TABLE_ROW = preload("res://ui/components/truth_table_row.tscn")
+const DARK_PANEL = preload("res://ui/themes/darkPanel.tres")
+const LIGHT_PANEL = preload("res://ui/themes/lightPanel.tres")
+
 var close_pos: Vector2 = Vector2(1141, 135)
 var open_pos: Vector2 = Vector2(867, 135)
 var opened: bool = false
 
 @onready var debounce_timer: Timer = $DebounceTimer
+@onready var truth_table_heading: Panel = $TruthTableHeading
+@onready var truth_table: VBoxContainer = $TruthTableScroll/VBoxContainer
+
 
 func _ready() -> void:
 	self.position = close_pos
+	
+	var level_data : Dictionary = DataManager.current_level.data
+	
+	var output_count : int = int(level_data["end_count"])
+	var input_count : int = len(level_data["truth_table"][0]) - output_count
+	
+	truth_table_heading.clear()
+	var heading_count: int = 0
+	for input in input_count:
+		truth_table_heading.variables += String.chr(65  + heading_count)
+		heading_count += 1
+	for output in output_count:
+		truth_table_heading.outputs += String.chr(65  + heading_count)
+		heading_count += 1
+	truth_table_heading.set_data()
+	
+	# remove current data
+	var truth_table_rows: Array[Node] = truth_table.get_children()
+	for row in truth_table_rows:
+		truth_table.remove_child(row)
+	
+	var row_count: int = 0
+	for row : String in level_data["truth_table"]:
+		var new_row: Panel = TRUTH_TABLE_ROW.instantiate()
+		new_row.variables = row.left(input_count)
+		new_row.outputs = row.right(output_count)
+		
+		if row_count % 2 == 0:
+			new_row.theme = DARK_PANEL
+		else:
+			new_row.theme = LIGHT_PANEL
+		row_count += 1
+		truth_table.add_child(new_row)
 
 
 func _on_handle_button_pressed() -> void:
